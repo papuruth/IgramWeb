b493d1d5f4d590ef93e23b88c0bc6e7b24993657
Papu Kumar <papu.kumar@kelltontech.com>
2021-09-01 20:58:03 +0530

    fix: #changed backend base url

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3001'
-    : 'https://igramweb.herokuapp.com';
+    : 'https://igramwebserver.herokuapp.com';
